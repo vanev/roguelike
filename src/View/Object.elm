b26e27a5f3078610c2af584e37c9152575960829
Evan Siegel <evan@grailed.com>
2018-01-07 08:20:31 -0500

    Cleanup

--- src/View/Object.elm
+++ src/View/Object.elm
@@ -1,7 +1,6 @@
 module View.Object exposing (render)
 
 import Html exposing (Html, div)
-import Html.Attributes exposing (classList, style)
 import Messages exposing (Msg)
 import Game.Object exposing (Object)
 

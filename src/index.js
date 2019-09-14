f8c6415b820c00716fc2b30802f5a8315f8c6c96
Evan Siegel <siegel.evan@gmail.com>
2019-09-14 16:23:09 -0400

    Switches to Parcel

--- src/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-require('./index.html')
-require('./styles/index.scss')
-
-const { Elm } = require('./Main.elm')
-const node = document.getElementById('app');
-
-const app = Elm.Main.init({ node })
